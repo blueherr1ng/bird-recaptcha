c149303bc63e40604e1e988c6c2531e70b3019c4
Mirabelle Feng <mirabellefeng@gmail.com>
2026-03-11 09:11:21 -0400

    new birds

--- src/Components.jsx
+++ src/Components.jsx
@@ -3,89 +3,8 @@ import { TbReload } from "react-icons/tb";
 import { MdHeadphones } from "react-icons/md";
 import { MdInfoOutline } from "react-icons/md";
 import { useState } from 'react';
+import { IMAGES } from './images';
 
-const IMAGES = [
-  {
-    id: 1,
-    image: '/birds/willowptarmigon.png',
-    solution: [10, 11, 14, 15]
-  },
-  {
-    id: 2,
-    image: '/birds/roseringedparakeet.png',
-    solution: [4, 5, 8, 9]
-  },
-  {
-    id: 3,
-    image: '/birds/sandgrouse.png',
-    solution: [8, 9, 10]
-  },
-  {
-    id: 4,
-    image: '/birds/sonimcrine.jpg',
-    solution: []
-  },
-  {
-    id: 5,
-    image: '/birds/hummingbird.jpg',
-    solution: [5,9]
-  },
-  {
-    id: 6,
-    image: '/birds/unsure.JPG',
-    solution: [1,2,5,6]
-  },
-  {
-    id: 7,
-    image: '/birds/hawkmaybe.jpg',
-    solution: [5,9]
-  },
-  {
-    id: 8,
-    image: '/birds/dontknow.jpg',
-    solution: [5,6,9,10]
-  },
-  {
-    id: 9,
-    image: '/birds/bushtit.jpg',
-    solution: [8,9]
-  },
-  {
-    id: 10,
-    image: '/birds/chickadee.jpg',
-    solution: [5,8,9]
-  },
-  {
-    id: 11,
-    image: '/birds/bittern.png',
-    solution: [10,14]
-  },
-  {
-    id: 12,
-    image: '/birds/kestrel.png',
-    solution: [6,10]
-  },
-  {
-    id: 13,
-    image: '/birds/eurasianwoodcock.png',
-    solution: [4,5,8,9]
-  },
-  {
-    id: 14,
-    image: '/birds/greathornedowl.png',
-    solution: [6,10,14]
-  },
-  {
-    id: 15,
-    image: '/birds/nightjar.png',
-    solution: [6,7,9,10,11]
-  },
-  {
-    id: 16,
-    image: '/birds/notinskillset.png',
-    solution: [5,9,13]
-  },
-];
 
 function RecaptchaHeader() {
     return (
@@ -199,8 +118,8 @@ function Gallery() {
     <p> a collection of camouflaged or just poorly photographed birds - try to find them!</p>
     <div className="gallery-container">
       {IMAGES.map((item) => (
-        <div key={item.id} className="gallery-item">
-          <img src={item.image} alt="bird" style={{ width: '200px', margin: '10px' }} />
+        <div key={item.id}>
+          <img src={item.image} alt="bird" style={{ width: '300px', margin: '10px' }} />
           <p>{item.image}</p>
         </div>
       ))}
---
+++ src/images.js
@@ -0,0 +1,107 @@
+export const IMAGES = [
+  {
+    id: 1,
+    image: "/birds/willowptarmigon.png",
+    solution: [10, 14, 15],
+  },
+  {
+    id: 2,
+    image: "/birds/roseringedparakeet.png",
+    solution: [4, 5, 8, 9],
+  },
+  {
+    id: 3,
+    image: "/birds/sandgrouse.png",
+    solution: [8, 9, 10],
+  },
+  {
+    id: 4,
+    image: "/birds/sonimcrine.jpg",
+    solution: [],
+  },
+  {
+    id: 5,
+    image: "/birds/hummingbird.jpg",
+    solution: [5, 9],
+  },
+  {
+    id: 6,
+    image: "/birds/unsure.JPG",
+    solution: [1, 2, 5, 6],
+  },
+  {
+    id: 7,
+    image: "/birds/hawkmaybe.jpg",
+    solution: [5, 9],
+  },
+  {
+    id: 8,
+    image: "/birds/dontknow.jpg",
+    solution: [5, 6, 9, 10],
+  },
+  {
+    id: 9,
+    image: "/birds/bushtit.jpg",
+    solution: [8, 9],
+  },
+  {
+    id: 10,
+    image: "/birds/chickadee.jpg",
+    solution: [5, 8, 9],
+  },
+  {
+    id: 11,
+    image: "/birds/bittern.png",
+    solution: [10, 14],
+  },
+  {
+    id: 12,
+    image: "/birds/kestrel.png",
+    solution: [6, 10],
+  },
+  {
+    id: 13,
+    image: "/birds/eurasianwoodcock.png",
+    solution: [4, 5, 8, 9],
+  },
+  {
+    id: 14,
+    image: "/birds/greathornedowl.png",
+    solution: [6, 10, 14],
+  },
+  {
+    id: 15,
+    image: "/birds/nightjar.png",
+    solution: [6, 7, 9, 10, 11],
+  },
+  {
+    id: 16,
+    image: "/birds/notinskillset.png",
+    solution: [5, 9, 13],
+  },
+  {
+    id: 17,
+    image: "/birds/hummingbird3.jpg",
+    solution: [9],
+  },
+  {
+    id: 18,
+    image: "/birds/snowyegrets.jpg",
+    solution: [8, 9, 10, 11],
+  },
+  {
+    id: 19,
+    image: "/birds/snowyegret.jpg",
+    solution: [10],
+  },
+  {
+    id: 20,
+    image: "/birds/greatblueheron.jpg",
+    solution: [10],
+  },
+  {
+    id: 21,
+    image: "/birds/seagull.jpg",
+    solution: [1, 5],
+  },
+];
